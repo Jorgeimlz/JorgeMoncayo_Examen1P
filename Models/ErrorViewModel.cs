cce5b5e36e3c1a3e6b7c667651a1087aea5d7bef
Jorge Isaac Moncayo Loaiza <jorge.moncayo.loaiza@udla.edu.ec>
2023-10-25 07:22:30 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JorgeMoncayo_Examen1P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
