4251f04353dff1778c158a847422f13cff05e3d4
Jorge Isaac Moncayo Loaiza <jorge.moncayo.loaiza@udla.edu.ec>
2023-10-25 07:41:53 -0500

    Commit creacion de la tabla y 6 atributos de tipo int, string, bool, bool, decimal, Date

---
+++ Models/JorgeMoncayo_tabla.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JorgeMoncayo_Examen1P.Models
+{
+    public class JorgeMoncayo_tabla
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? NombreJuego { get; set; }
+        public bool Ps4 {  get; set; }
+
+        public bool Ps5 { get; set; }
+        public decimal Precio { get; set; }
+
+        public DateTime? PrecioDate { get; set;}
+    }
+
+
+
+  
+}
